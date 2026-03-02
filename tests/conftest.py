bfd17501f8d8ad98a9c09b9045cb7de3c164a1e9
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2026-03-02 08:09:27 +0100

    Improve build

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
+import urllib.request
+
 import pytest
-import requests
 
 from krcg import config
 from krcg_api import create_app
@@ -10,12 +11,12 @@ from fastapi.testclient import TestClient
 def pytest_sessionstart(session):
     # Do not launch tests is there is no proper Internet connection.
     try:
-        requests.get("http://www.google.com", timeout=1)
-    except requests.exceptions.RequestException:
+        urllib.request.urlopen("http://www.google.com", timeout=1)
+    except OSError:
         pytest.fail("No internet connection")
     try:
-        requests.get(config.KRCG_STATIC_SERVER, timeout=1)
-    except requests.exceptions.RequestException:
+        urllib.request.urlopen(config.KRCG_STATIC_SERVER, timeout=1)
+    except OSError:
         pytest.fail("KRCG website not available")
 
 
